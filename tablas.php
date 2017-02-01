9c62f325e402f7c1652e9def216d81d93fa967a6
Julio César <julioc255io@gmail.com>
2017-02-01 16:48:47 -0600

    add tables

--- tablas.php
+++ tablas.php
@@ -19,4 +19,25 @@ echo "<table border=\"1\">";
 
 
 
-  
+  <?php
+$start = 1;
+$end = 10;
+
+echo ‘<table border=”1″ cellspacing=”0″ cellpadding=”5″>’;
+for ($y = $start – 1; $y <= $end; ++$y) {
+echo “<tr>”;
+for ($x = $start – 1; $x <= $end; ++$x) {
+if ($y == $start – 1) {
+echo “<td><b>$x</b></td>”;
+
+} else if ($x == $start – 1) {
+echo “<td><b>$y</b></td>”;
+} else {
+echo “<td>”;
+echo $x*$y;
+echo “</td>”;
+}
+}
+print “</tr>”;
+}
+print “</table>”;?>
