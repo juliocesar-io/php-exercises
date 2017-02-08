57c5bec990516541de0fbbe31c0e9843ef709f58
Julio César <julioc255io@gmail.com>
2017-02-08 15:21:45 -0600

    class 3

---
+++ multiarrays.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JulioC
+ * Date: 2/8/17
+ * Time: 15:10
+ */
+
+$a = array();
+for($c=0; $c<5; $c++){
+    $a[$c] = array();
+    for($r=0; $r<3; $r++){
+        $a[$c][$r] = rand();
+    }
+}
+
+print_r($a);
+
+
+function getPrice($v, $iva = 0.16){
+
+    $total = $v + ($v * $iva);
+
+    echo $total;
+}
+
+getPrice(7000);
+
